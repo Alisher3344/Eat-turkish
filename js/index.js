4fb845c477223220563e8cb1acfefc64d092e8c0
Alisher3344 <alisher3344ali@gmail.com>
2024-07-09 18:20:51 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+    var owl = $('.owl-carousel');
+ owl.owlCarousel({
+    items :4,
+      loop: true,
+      margin: 20,
+      nav: false,
+      autoplayTimeout:2500,
+    autoplay:true,
+    autoplayHoverPause:true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600:{
+            items:1,
+        },
+        900: {
+          items: 3 ,
+        },
+       1200:{
+        items:4,
+       }
+  
+      },
+    });
+    
