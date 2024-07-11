ccf7537a76c687d434b528cdf9b3d898c55623b9
Alisher3344 <alisher3344ali@gmail.com>
2024-07-11 16:46:04 +0500

    ddffdbd

--- js/index.js
+++ js/index.js
@@ -43,3 +43,58 @@
     }, false)
   })
 })()
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+let navbar2 = document.getElementById("navbar2");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+     navbar2.classList.add("navbar-shrink2");
+  } else {
+  navbar.classList.remove("navbar-shrink");
+   navbar2.classList.remove("navbar-shrink2");
+ }
+}
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+
+
+
+ let modeBtn2 = document.getElementById("mode-btn2");
+  modeBtn2.addEventListener("click", function () {
+      if (document.body.className !="darke2"){
+              this.firstElementChild.src = "../imgs/dark-theme.svg";
+      }
+      else{
+          this.firstElementChild.src = "../imgs/dark-theme.svg";
+  } 
+  document.body.classList.toggle("darke2");
+  }
+  );  
+
+  
+  
+  AOS.init();
+
+
+
+
+
+
+
